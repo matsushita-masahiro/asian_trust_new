533ad07ce495fdcfa90f00d32a23f13b727df9a1
Ubuntu <ubuntu@ip-172-31-38-40.ap-northeast-1.compute.internal>
2025-06-25 23:40:28 +0000

    force_html_middleware.rb module Middleware

--- lib/middleware/force_html_middleware.rb
+++ lib/middleware/force_html_middleware.rb
@@ -1,11 +1,16 @@
-# lib/middleware/force_html_middleware.rb
-class ForceHtmlMiddleware
-  def initialize(app)
-    @app = app
-  end
 
-  def call(env)
-    env["HTTP_ACCEPT"] = "text/html" if env["HTTP_ACCEPT"].to_s !~ /html/
-    @app.call(env)
+# lib/middleware/force_html_middleware.rb
+
+module Middleware
+  class ForceHtmlMiddleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      env["HTTP_ACCEPT"] = "text/html" if env["HTTP_ACCEPT"].to_s !~ /html/
+      @app.call(env)
+    end
   end
 end
+

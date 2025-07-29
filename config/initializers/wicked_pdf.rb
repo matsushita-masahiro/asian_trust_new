be78d37d5d31b109b25167b966c13d1a452a0068
松下昌弘 <matsushitamasahiro@matsushitamasahironoMacBook-Air-2.local>
2025-07-30 02:10:27 +0900

    Fix wicked_pdf config and purge Heroku cache

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 WickedPdf.config ||= {}
 
-if Rails.env.production?
-  WickedPdf.config.merge!(
-    exe_path: '/app/bin/wkhtmltopdf' # Herokuのパス
-  )
-else
-  # 開発環境用パス（必要に応じて変更）
-  WickedPdf.config.merge!(
-    exe_path: `which wkhtmltopdf`.strip
-  )
-end
+WickedPdf.config.merge!(
+  exe_path: if Rails.env.production?
+              # Heroku 用：wkhtmltopdf-heroku ビルドパス
+              '/app/bin/wkhtmltopdf'
+            else
+              # 開発環境：which でパス取得
+              `which wkhtmltopdf`.strip
+            end
+)

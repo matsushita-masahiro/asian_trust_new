3bff909f2614167fc54708cdf9c187cbf7973a38
松下昌弘 <matsushitamasahiro@matsushitamasahironoMacBook-Air-2.local>
2025-07-30 02:00:52 +0900

    Fix wicked_pdf config for Heroku wkhtmltopdf path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,12 @@
-WickedPdf.configure do |c|
-  c.exe_path = Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+WickedPdf.config ||= {}
+
+if Rails.env.production?
+  WickedPdf.config.merge!(
+    exe_path: '/app/bin/wkhtmltopdf' # Herokuのパス
+  )
+else
+  # 開発環境用パス（必要に応じて変更）
+  WickedPdf.config.merge!(
+    exe_path: `which wkhtmltopdf`.strip
+  )
 end

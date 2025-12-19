9b47710b45c8ef9d3d8f0c3e43d330da76acff8a
Masahiro Matsushita <matsushita.samurai@gmail.com>
2025-12-20 03:30:32 +0900

    Fix Propshaft load path for builds CSS

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+# IMPORTANT:
+# Make Propshaft able to find CSS built by cssbundling-rails (app/assets/builds).
+Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
